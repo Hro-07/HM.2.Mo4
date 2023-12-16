480ec47555f9120cd62bb82095a79bd589f95222
Hro-07 <hrostepanyan5@gmail.com>
2023-12-16 13:07:56 +0200

    Initial Commit

---
+++ HM.2.Mo4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HM.2.Mo4
+//
+//  Created by Hrayr on 16/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HM.2.Mo4/HM_2_Mo4App.swift
@@ -0,0 +1,17 @@
+//
+//  HM_2_Mo4App.swift
+//  HM.2.Mo4
+//
+//  Created by Hrayr on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HM_2_Mo4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
